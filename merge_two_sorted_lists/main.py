a3d4ce12536669f86eee350a68db25bfd29e7d9d
jakeless <jacobtaylormoore@gmail.com>
2022-06-25 12:44:39 -0500

    template for merge lists

---
+++ merge_two_sorted_lists/main.py
@@ -0,0 +1,21 @@
+# Author:      Jacob Moore
+# Date:        6/25/22
+# Description: Leetcode question #21 merge two sorted lists
+
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution(object):
+    def __init__(self):
+        self.merged = ListNode()
+
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+
